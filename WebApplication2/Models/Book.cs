66d7729ecf4c5bcf2d22ab69920e45aa6135760f
Youssef <youssef.afify122@gmail.com>
2021-06-11 00:33:44 +0200

    make some changes

---
+++ WebApplication2/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Book
+    {
+        public string title { set; get; }
+        public string desc { set; get; }
+        public string author { set; get; }
+        public string image { set; get; }
+        public double price { set; get; }
+        
+    }
+}
