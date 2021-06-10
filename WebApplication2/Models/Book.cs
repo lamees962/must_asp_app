069cc817a61a4d35d48d462776d77da2dfa9507d
Youssef <youssef.afify122@gmail.com>
2021-06-11 01:05:20 +0200

    update books.cs

--- WebApplication2/Models/Book.cs
+++ WebApplication2/Models/Book.cs
@@ -1,11 +1,24 @@
-﻿namespace WebApplication2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+
+        [Required]
         public string title { set; get; }
+
+        [Required]
         public string desc { set; get; }
+
+        [Required]
         public string author { set; get; }
+       
+        [Required]
         public string image { set; get; }
+        
+        [Required]
         public double price { set; get; }
         
     }
