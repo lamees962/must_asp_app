28e8a745058ed1e8fcd94633d26c07876c106b39
lameesashraf <lameesashraf962@gmail.com>
2021-06-10 22:43:46 +0200

    hi github

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Cart ()
+        {
+            var v = new Product()
+            {
+                name = "Introduction to database",
+                quantity = 10,
+                image = "Product_Image",
+                price =240
+            };
+            ViewBag.Message = "Welcome to your cart.";
+
+            return View(v);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int quantity { set; get; }
+        public string image { set; get; }
+        public int price { set; get; }
+        public string name {set; get;}
+    }
+}
